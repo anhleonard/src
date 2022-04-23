c61b036bce9bfe9b965de15851961fde88294c24
Phan Trung <anhleonard2022002@gmail.com>
2022-04-23 18:35:17 +0700

    finish-commit

--- VueJS_Lesson2/src/App.vue
+++ VueJS_Lesson2/src/App.vue
@@ -1,12 +1,10 @@
 <template>
 <div class="parent">
-  <HeaderList v-bind:bags="bags"></HeaderList>
-  <ContentList v-bind:items="items"></ContentList>
+  <ContentList v-bind:items="items" v-bind:bags="bags"></ContentList>
 </div>
 </template>
 
 <script>
-import HeaderList from './assets/components/HeaderList.vue';
 import ContentList from './assets/components/ContentList.vue';
 export default {
   name: 'App',
@@ -22,30 +20,29 @@ export default {
         { id: 6, title: 'WINE & COCKTAILS'},
       ],
       items: [
-        { index: 0, text: 'ALMA – Coming Soon', img: 'https://thegrovela.com/wp-content/uploads/2021/04/GAF_Alma-Cocina-de-Mexico_Coming-Soon_900x600.jpg'},
-        { index: 1, text: 'AMC Theatres', img: 'https://thegrovela.com/wp-content/uploads/2021/07/2021_GAF_AMC_PromoImage_Hero_HP-Tile_900x600.png'},
-        { index: 2, text: 'Bar Verde at Nordstrom', img: 'https://thegrovela.com/wp-content/uploads/2017/05/BarVerde_900x600.jpg'},
-        { index: 3, text: 'Blue Ribbon Sushi Bar & Grill', img: 'https://thegrovela.com/wp-content/uploads/2017/05/BlueRibbon_900x600.jpg'},
-        { index: 4, text: 'Chill Since ‘93 – Now Open', img: 'https://thegrovela.com/wp-content/uploads/2021/12/GAF_ChillSince93_NowOpen_900x600.jpg'},
-        { index: 5, text: 'edo bites', img: 'https://thegrovela.com/wp-content/uploads/2019/09/GAF_Edo_NowOpen_900x600.jpg'},
-        { index: 6, text: 'Groundwork Coffee', img: 'https://thegrovela.com/wp-content/uploads/2017/05/Grove_Dining_Dining-Detail_Groundwork-Coffee_Thumb-900x600.png'},
-        { index: 7, text: 'Häagen-Dazs', img: 'https://thegrovela.com/wp-content/uploads/2017/05/Grove_Dining_Dining_Detail_Haagen-Dazs_900x600.png'},
-        { index: 8, text: 'La Piazza', img: 'https://thegrovela.com/wp-content/uploads/2017/05/GAF_LaPiazza_900x600.jpg'},
-        { index: 9, text: 'Maggiano’s Little Italy', img: 'https://thegrovela.com/wp-content/uploads/2017/05/Maggianos_900x600.jpg'},
-        { index: 10, text: 'Nordstrom eBar', img: 'https://thegrovela.com/wp-content/uploads/2017/05/Grove_Dining_Dining-Detail_Nordstrom-Ebar-900x600.png'},
-        { index: 11, text: 'Pressed', img: 'https://thegrovela.com/wp-content/uploads/2017/05/2021_AAB_PressedJuicery_TenantPage_HP-Tile_900x600-1.png'},
-        { index: 12, text: 'See’s Candies', img: 'https://thegrovela.com/wp-content/uploads/2017/10/2017-HW-DA-The-Grove-LA-Shop-AY-900x600-FINAL.jpg'},
-        { index: 13, text: 'Sprinkles Cupcakes', img: 'https://thegrovela.com/wp-content/uploads/2017/05/Sprinkles_900x600.jpg'},
-        { index: 14, text: 'Starbucks at Barnes & Noble', img: 'https://thegrovela.com/wp-content/uploads/2017/06/Grove_Dining_Dining-Detail_Barnes-Noble-Cafe_1-copy_crop-P_900x600px.png'},
-        { index: 15, text: 'The Cheesecake Factory', img: 'https://thegrovela.com/wp-content/uploads/2017/05/Grove_Dining_Dining-Detail_The-Cheesecake-Factory-900x600.png'},
-        { index: 16, text: 'The Fountain Bar', img: 'https://thegrovela.com/wp-content/uploads/2017/09/FountainBar_900x600.jpg'},
-        { index: 17, text: 'Umami Burger', img: 'https://thegrovela.com/wp-content/uploads/2017/05/GAF_Umami-Burger-Dining-Page_900x600.jpg'},
-        { index: 18, text: 'Wetzel’s Pretzels', img: 'https://thegrovela.com/wp-content/uploads/2017/05/Grove_Dining_Dining-Detail_Wetzels-Pretzels.jpg'}
+        { index: 1, title: 'ALL', text: 'ALMA – Coming Soon', img: 'https://thegrovela.com/wp-content/uploads/2021/04/GAF_Alma-Cocina-de-Mexico_Coming-Soon_900x600.jpg'},
+        { index: 2, title: 'ALL', title: 'QUICK BITES', title: 'FAMILY-FRIENDLY DINING', text: 'AMC Theatres', img: 'https://thegrovela.com/wp-content/uploads/2021/07/2021_GAF_AMC_PromoImage_Hero_HP-Tile_900x600.png'},
+        { index: 3, title: 'ALL', title: 'FAMILY-FRIENDLY DINING', text: 'Bar Verde at Nordstrom', img: 'https://thegrovela.com/wp-content/uploads/2017/05/BarVerde_900x600.jpg'},
+        { index: 4, title: 'ALL', text: 'Blue Ribbon Sushi Bar & Grill', img: 'https://thegrovela.com/wp-content/uploads/2017/05/BlueRibbon_900x600.jpg'},
+        { index: 5, title: 'ALL', title: 'FAMILY-FRIENDLY DINING', text: 'Chill Since ‘93 – Now Open', img: 'https://thegrovela.com/wp-content/uploads/2021/12/GAF_ChillSince93_NowOpen_900x600.jpg'},
+        { index: 6, title: 'ALL', text: 'edo bites', img: 'https://thegrovela.com/wp-content/uploads/2019/09/GAF_Edo_NowOpen_900x600.jpg'},
+        { index: 7, title: 'ALL', title: 'COFFEE & DESSERTS', text: 'Groundwork Coffee', img: 'https://thegrovela.com/wp-content/uploads/2017/05/Grove_Dining_Dining-Detail_Groundwork-Coffee_Thumb-900x600.png'},
+        { index: 8, title: 'ALL', title: 'COFFEE & DESSERTS', text: 'Häagen-Dazs', img: 'https://thegrovela.com/wp-content/uploads/2017/05/Grove_Dining_Dining_Detail_Haagen-Dazs_900x600.png'},
+        { index: 9, title: 'ALL', title: 'FAMILY-FRIENDLY DINING', text: 'La Piazza', img: 'https://thegrovela.com/wp-content/uploads/2017/05/GAF_LaPiazza_900x600.jpg'},
+        { index: 10, title: 'ALL', title: 'COFFEE & DESSERTS', title: 'FAMILY-FRIENDLY DINING', text: 'Maggiano’s Little Italy', img: 'https://thegrovela.com/wp-content/uploads/2017/05/Maggianos_900x600.jpg'},
+        { index: 11, title: 'ALL', title: 'COFFEE & DESSERTS', text: 'Nordstrom eBar', img: 'https://thegrovela.com/wp-content/uploads/2017/05/Grove_Dining_Dining-Detail_Nordstrom-Ebar-900x600.png'},
+        { index: 12, title: 'ALL', title: 'QUICK BITES', text: 'Pressed', img: 'https://thegrovela.com/wp-content/uploads/2017/05/2021_AAB_PressedJuicery_TenantPage_HP-Tile_900x600-1.png'},
+        { index: 13, title: 'ALL', title: 'COFFEE & DESSERTS', text: 'See’s Candies', img: 'https://thegrovela.com/wp-content/uploads/2017/10/2017-HW-DA-The-Grove-LA-Shop-AY-900x600-FINAL.jpg'},
+        { index: 14, title: 'ALL', title: 'COFFEE & DESSERTS', text: 'Sprinkles Cupcakes', img: 'https://thegrovela.com/wp-content/uploads/2017/05/Sprinkles_900x600.jpg'},
+        { index: 15, title: 'ALL', title: 'QUICK BITES', title: 'COFFEE & DESSERTS', text: 'Starbucks at Barnes & Noble', img: 'https://thegrovela.com/wp-content/uploads/2017/06/Grove_Dining_Dining-Detail_Barnes-Noble-Cafe_1-copy_crop-P_900x600px.png'},
+        { index: 16, title: 'ALL', title: 'COFFEE & DESSERTS', title: 'FAMILY-FRIENDLY DINING', text: 'The Cheesecake Factory', img: 'https://thegrovela.com/wp-content/uploads/2017/05/Grove_Dining_Dining-Detail_The-Cheesecake-Factory-900x600.png'},
+        { index: 17, title: 'ALL', title: 'QUICK BITES', text: 'The Fountain Bar', img: 'https://thegrovela.com/wp-content/uploads/2017/09/FountainBar_900x600.jpg'},
+        { index: 18, title: 'ALL', title: 'QUICK BITES', title: 'FAMILY-FRIENDLY DINING',text: 'Umami Burger', img: 'https://thegrovela.com/wp-content/uploads/2017/05/GAF_Umami-Burger-Dining-Page_900x600.jpg'},
+        { index: 19, title: 'ALL', title: 'QUICK BITES', title: 'COFFEE & DESSERTS', text: 'Wetzel’s Pretzels', img: 'https://thegrovela.com/wp-content/uploads/2017/05/Grove_Dining_Dining-Detail_Wetzels-Pretzels.jpg'}
       ]
     }
   },
   components: {
-    HeaderList,
     ContentList
   }
 }
--- VueJS_Lesson2/src/assets/components/ContentList.vue
+++ VueJS_Lesson2/src/assets/components/ContentList.vue
@@ -1,10 +1,22 @@
 <template>
 <main>
+<div class="header">
+    <div class="header__form">
+          <div class="header__btn">
+              <h4>ALL</h4>
+              <font-awesome-icon  class="header__btn-icon" icon="fa-solid fa-angle-down" v-on:click="myFunction()"/>
+          </div>
+          <div class="header__list">
+              <div class="header__item" v-for="bag in bags" v-bind:key="bag.id" v-on:click="getValue(bag)">
+                  {{bag.title}}
+              </div>
+          </div>
+    </div>
     <div class="main__wrapper">
         <div class="main__list-item">
-            <div class="main__item" v-for="item in items" v-bind:key="item.index">
+            <div class="main__item" v-for="item in filterItems" v-bind:key="item.index">
                 <div class="main__item-img">
-                    <img src=" {{item.img}} " alt="">
+                    <img class="main__item-img-link" :src="item.img" alt="">
                 </div>
                 <div class="main__item-name">
                     {{item.text}}
@@ -12,23 +24,122 @@
             </div>
         </div>
     </div>
+</div>
 </main>
 </template>
 
 <script>
-import {EventBus} from './event-bus.js';
+
 export default {
     name: 'ContentList',
-    props: ["items"],
+    props: {
+        bags: Array,
+        items: Array
+    },
     data() {
         return {
-            message: ''
+            active: false,
+            filterItems: []
         }
     },
-    
+    methods: {
+        getValue(bag) {
+            this.items.map( item => {
+                this.filterItems = this.items.filter(checkItem);
+                function checkItem(item) {
+                    if(bag.title === item.title) return item;
+                }
+            });
+        },
+        myFunction() {
+            this.active = !this.active;
+            if(this.active === true) {
+                var value = document.querySelector('.header__list');
+                value.style.display = 'block';
+            }
+            else {
+                var value = document.querySelector('.header__list');
+                value.style.display = 'none';
+            }
+        }
+    },
+    created () {
+        this.filterItems = this.items
+    }
 }
 </script>
 
 <style>
-
+/* header */
+.header {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.header__form {
+    position: relative;
+    width: 280px;
+}
+.header__btn {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border: 1px solid #000;
+    padding: 15px;
+    width: 100%;
+    height: 30px;
+}
+.header__list {
+    width: inherit;
+    background: #fff;
+    z-index: 1000;
+    border: 1px solid #000;
+    position: absolute;
+    display: none;
+}
+.header__item {
+    margin: 15px;
+}
+.header__item:hover {
+    text-decoration: underline;
+    cursor: pointer;
+}
+.header__btn-icon:hover {
+    cursor: pointer;
+}
+/* main */
+.main__list-item {
+    margin: 40px auto;
+    width: 1300px;
+    display: flex;
+    flex-wrap: wrap;
+}
+.main__item {
+    padding: 20px;
+    width: 25%;
+    /* display: none; */
+}
+.main__item-img {
+    overflow: hidden;
+    width: 100%;
+    margin-bottom: 20px;
+}
+.main__item:hover .main__item-img-link {
+    transform: scale(1.05);
+    cursor: pointer;
+    transition: all 0.3s ease-in-out;
+}
+.main__item:hover .main__item-name {
+    color: green;
+}
+.main__item-img-link {
+    width: 100%;
+}
+.main__item-name {
+    font-weight: 1000;
+}
+.main__item-name:hover {
+    color: green;
+    cursor: pointer;
+}
 </style>
